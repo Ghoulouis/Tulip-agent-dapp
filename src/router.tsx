417019ffcc9098866e17ed58e1c5e2b4bd0c0b99
Ghoulouis <maikhanh3241@gmail.com>
2025-03-08 13:22:44 +0700

    change theme color dark -> light

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,11 @@ import { useRoutes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import HomeLayout from "./layout/layouts/HomeLayout";
 import Layout2 from "./layout/Layout2";
+import HomePage from "./views/HomePage";
 
 export default function RouterUrl() {
     return useRoutes([
-        { path: "*", element: <Layout2 children={<HomeLayout />} /> },
+        { path: "*", element: <Layout2 children={<HomePage />} /> },
         {
             path: "a",
             element: <Layout children={<HomeLayout />} />,
